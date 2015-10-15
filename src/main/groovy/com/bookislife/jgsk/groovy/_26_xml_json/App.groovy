72b8f624174527d1b80d37795bfc7d55fda745d8
mrseasons <mrseasons@mrseasonsdeMacBook-Pro.local>
2015-10-15 08:23:39 +0800

    Update.

--- src/main/groovy/com/bookislife/jgsk/groovy/_26_xml_json/App.groovy
+++ src/main/groovy/com/bookislife/jgsk/groovy/_26_xml_json/App.groovy
@@ -2,6 +2,8 @@ package com.bookislife.jgsk.groovy._26_xml_json
 
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
+import groovy.xml.MarkupBuilder
+import groovy.xml.StreamingMarkupBuilder
 
 /**
  * Created by SidneyXu on 2015/10/13.
@@ -11,9 +13,13 @@ class App {
     public static void main(String[] args) {
         createJson()
         parseJson()
+        createXml()
+        parseXml()
     }
 
     static def createJson() {
+        println('========invoke createJson()========')
+
         //  JSON Object
         def json = new JsonBuilder()
         json.call {
@@ -48,6 +54,8 @@ class App {
     }
 
     static def parseJson() {
+        println('========invoke parseJson()========')
+
         def json = """
 {
     "data": [
@@ -65,5 +73,69 @@ class App {
         def result = new JsonSlurper().parseText(json)
         println result.data[0].code     //  404
         println result.data[1].value    //  redirect
+    }
+
+    static def createXml() {
+        println('========invoke createXml()========')
+
+        //  MarkupBuilder
+        def sw = new StringWriter()
+        def xml = new MarkupBuilder(sw)
+        xml.langs(type: "current") {
+            language("Java")
+            language("Groovy")
+            language("Scala")
+            language("Kotlin")
+        }
+        println(sw)
+
+        //  StreamingMarkupBuilder
+        def comment = "<![CDATA[<!-- address is new to this release -->]]>"
+        def builder = new StreamingMarkupBuilder()
+        builder.encoding = "UTF-8"
+        def person = {
+            mkp.xmlDeclaration()
+            mkp.pi("xml-stylesheet": "type='text/xsl' href='myfile.xslt'")
+            mkp.declareNamespace('': 'http://myDefaultNamespace')
+            mkp.declareNamespace('location': 'http://someOtherNamespace')
+            person(id: 100) {
+                firstname("Jane")
+                lastname("Doe")
+                mkp.yieldUnescaped(comment)
+                location.address("123 Main")
+            }
+        }
+        def writer = new PrintWriter(System.out)
+        writer << builder.bind(person)
+        println()
+    }
+
+    static def parseXml() {
+        println('========invoke parseXml()========')
+
+        //  XmlParser
+        def xml = """
+<langs type='current' count='4' mainstream='true'>
+  <language flavor='static' version='1.8.0_25'>Java</language>
+  <language flavor='dynamic' version='2.4.4'>Groovy</language>
+  <language flavor='static' version='2.11.5'>Scala</language>
+  <language flavor='static' version='0.12.613'>Kotlin</language>
+</langs>
+"""
+        def langs = new XmlParser().parseText(xml)
+        println "count=${langs.attribute("count")}" //  count=4
+        println "count=${langs.@count}"
+        langs.language.each {
+            println it.text()
+        }
+        println()
+
+        //  XmlSlurper
+        langs = new XmlSlurper().parseText(xml)
+        println "count=${langs.@count}" //  count=4
+        langs.language.each {
+            println it.text()
+        }
+        println langs.language[1].@flavor   //  dynamic
     }
 }
--- src/main/java/com/bookislife/jgsk/java/_26_xml_json/App.java
+++ src/main/java/com/bookislife/jgsk/java/_26_xml_json/App.java
@@ -3,8 +3,22 @@ package com.bookislife.jgsk.java._26_xml_json;
 import com.oracle.javafx.jmx.json.JSONDocument;
 import com.oracle.javafx.jmx.json.JSONReader;
 import com.oracle.javafx.jmx.json.impl.JSONStreamReaderImpl;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
 
-import java.io.StringReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 
 /**
  * Created by SidneyXu on 2015/10/13.
@@ -14,9 +28,13 @@ public class App {
     public static void main(String[] args) {
         createJson();
         parseJson();
+        createXml();
+        parseXml();
     }
 
     private static void createJson() {
+        System.out.println("========invoke createJson()========");
+
         //  JSON Object
         JSONDocument jsonDocument = JSONDocument.createObject();
         JSONDocument results = JSONDocument.createObject();
@@ -37,6 +55,8 @@ public class App {
     }
 
     private static void parseJson() {
+        System.out.println("========invoke parseJson()========");
+
         String json = "{\"data\":[{\"404\":\"not found\"},{\"302\":\"redirect\"}]}";
         JSONReader jsonReader = new JSONStreamReaderImpl(new StringReader(json));
         JSONDocument jsonDocument = jsonReader.build();
@@ -45,4 +65,84 @@ public class App {
         System.out.println(data.get(1).object().values().iterator().next());    //  redirect
     }
 
+    private static void createXml() {
+        System.out.println("========invoke createXml()========");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.newDocument();
+
+            Element langs = doc.createElement("langs");
+            langs.setAttribute("type", "current");
+            doc.appendChild(langs);
+
+            Element language1 = doc.createElement("language");
+            Text text1 = doc.createTextNode("Java");
+            language1.appendChild(text1);
+            langs.appendChild(language1);
+
+            Element language2 = doc.createElement("language");
+            Text text2 = doc.createTextNode("Groovy");
+            language2.appendChild(text2);
+            langs.appendChild(language2);
+
+            Element language3 = doc.createElement("language");
+            Text text3 = doc.createTextNode("Scala");
+            language3.appendChild(text3);
+            langs.appendChild(language3);
+
+            Element language4 = doc.createElement("language");
+            Text text4 = doc.createTextNode("Kotlin");
+            language4.appendChild(text4);
+            langs.appendChild(language4);
+
+            // Output the XML
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.VERSION, "1.0");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            StringWriter writer = new StringWriter();
+            StreamResult streamResult = new StreamResult(writer);
+            DOMSource source = new DOMSource(doc);
+            transformer.transform(source, streamResult);
+            String xmlString = writer.toString();
+            System.out.println(xmlString);
+        } catch (ParserConfigurationException | TransformerException pce) {
+            pce.printStackTrace();
+        }
+    }
+
+    private static void parseXml() {
+        System.out.println("========invoke parseXml()========");
+
+        String xml = "<langs type=\"current\">\n" +
+                "  <language>Java</language>\n" +
+                "  <language>Groovy</language>\n" +
+                "  <language>JavaScript</language>\n" +
+                "</langs>";
+
+        byte[] xmlBytes = xml.getBytes();
+        InputStream is = new ByteArrayInputStream(xmlBytes);
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(is);
+
+            //print the "type" attribute
+            Element langs = doc.getDocumentElement();
+            System.out.println("type = " + langs.getAttribute("type"));
+
+            //print the "language" elements
+            NodeList list = langs.getElementsByTagName("language");
+            for (int i = 0; i < list.getLength(); i++) {
+                Element language = (Element) list.item(i);
+                System.out.println(language.getTextContent());
+            }
+        } catch (ParserConfigurationException | SAXException | IOException pce) {
+            pce.printStackTrace();
+        }
+    }
 }
