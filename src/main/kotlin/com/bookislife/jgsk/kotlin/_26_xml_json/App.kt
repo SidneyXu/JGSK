ed67b8361c9668529d5522278b94aeda416ed945
sidneyy <siriuseddy@gmail.com>
2015-10-16 17:43:34 +0800

    Update.

--- src/main/kotlin/com/bookislife/jgsk/kotlin/_26_xml_json/App.kt
+++ src/main/kotlin/com/bookislife/jgsk/kotlin/_26_xml_json/App.kt
@@ -1,6 +1,8 @@
 package com.bookislife.jgsk.kotlin._26_xml_json
 
-import java.io.File
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.io.StringReader
 import javax.xml.parsers.DocumentBuilderFactory
 import kotlin.dom.toXmlString
 
@@ -8,8 +10,18 @@ import kotlin.dom.toXmlString
  * Created by SidneyXu on 2015/10/13.
  */
 fun main(args: Array<String>) {
-    val f = File("coffeetime-kotlin/src/main/resources/weather.xml")
+    val xml = """
+<langs type='current' count='4' mainstream='true'>
+  <language flavor='static' version='1.8.0_25'>Java</language>
+  <language flavor='dynamic' version='2.4.4'>Groovy</language>
+  <language flavor='static' version='2.11.5'>Scala</language>
+  <language flavor='static' version='0.12.613'>Kotlin</language>
+</langs>
+"""
 
     val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-    println(builder.parse(f).toXmlString())
+    val doc = builder.parse(ByteArrayInputStream(xml.toByteArray()))
+    println(doc.toXmlString())
+
+//    println(doc.getElementsByTagName("langs").item(0).getAttributes().getNamedItem("count").getNodeValue())
 }
--- src/main/scala/com/bookislife/jgsk/scala/_26_xml_json/App.scala
+++ src/main/scala/com/bookislife/jgsk/scala/_26_xml_json/App.scala
@@ -1,107 +1,104 @@
 package com.bookislife.jgsk.scala._26_xml_json
 
 import scala.xml.dtd.{DocType, PublicID}
-import scala.xml.{NodeBuffer, PrettyPrinter, XML}
+import scala.xml.transform.{RewriteRule, RuleTransformer}
+import scala.xml.{Node, NodeBuffer, PrettyPrinter, XML}
 
 /**
  * Created by SidneyXu on 2015/10/13.
  */
 object App {
 
-    def main(args: Array[String]) {
-        createXml()
-        parseXml()
+  def main(args: Array[String]) {
+    createXml()
+    parseXml()
+  }
+
+  def createXml(): Unit = {
+    println("========invoke createXml()========")
+
+    //  Simple Creator
+    val langType = "current"
+    val langs =
+      <langs type={langType}>
+        <language>Java</language>
+        <language>Groovy</language>{scala.xml.PCData("<!-- Support Android-->")}<language>Scala</language>
+        <language>Kotlin</language>
+      </langs>
+    val printer = new PrettyPrinter(80, 4)
+    println(printer.format(langs))
+    println(scala.xml.Utility.trim(langs))
+
+    val langLst = List("Java", "Groovy", "Scala", "kotlin")
+    val xml =
+      <langs type={langType}>
+        {langLst.map(l =>
+        <language>
+          {l}
+        </language>
+      )}
+      </langs>
+    println(printer.format(xml))
+
+    //  NodeBuffer
+    var nb = new NodeBuffer
+    nb += <language>Java</language>
+    nb += <language>Groovy</language>
+    nb = nb &+ <language>Scala</language> &+ <language>Kotlin</language>
+    val langs3 =
+      <langs>
+        {nb}
+      </langs>
+    println(printer.format(xml))
+
+    //  Output
+    val doctype = DocType("html",
+      PublicID("-//W3C//DTD XHTML 1.0 Strict//EN",
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"),
+      Nil)
+    XML.save("files/langs.xml", langs,
+      "utf-8", xmlDecl = true, doctype)
+
+    val rewrite = new RuleTransformer(new RewriteRule {
+      override def transform(n: Node): Node =
+        n match {
+          case <language>{l}</language> =>
+            <language>{l}!!!</language>
+          case other => other
+        }
     }
+    )
+    val result = rewrite.transform(langs)
+    println(printer.format(result.head))
+  }
 
-    def createXml(): Unit = {
-        println("========invoke createXml()========")
+  def parseXml(): Unit = {
+    println("========invoke parseXml()========")
 
-        //  Simple Creator
-        val langType = "current"
-        val langs =
-            <langs type={langType}>
-                <language>Java</language>
-                <language>Groovy</language>
-                <language>Scala</language>
-                <language>Kotlin</language>
-            </langs>
-        val printer = new PrettyPrinter(80, 4)
-        println(printer.format(langs))
-        println(scala.xml.Utility.trim(langs))
+    val langs =
+      <langs type='current' count='4' mainstream='true'>
+        <language flavor='static' version='1.8.0_25'>
+          <title>Java</title>
+        </language>
+        <language flavor='dynamic' version='2.4.4'>
+          <title>Groovy</title>
+        </language>
+        <language flavor='static' version='2.11.5'>
+          <title>Scala</title>
+        </language>
+        <language flavor='static' version='0.12.613'>
+          <title>Kotlin</title>
+        </language>
+      </langs>
 
-        val langLst = List("Java", "Groovy", "Scala", "kotlin")
-        val xml =
-            <langs type={langType}>
-                {langLst.map(l =>
-                <language>
-                    {l}
-                </language>
-            )}
-            </langs>
-        println(printer.format(xml))
+    val java = (langs \ "language")(0)
+    val javaVersion = java \ "@version"
+    val javaTitle = (langs \\ "title")(0)
+    (langs \ "language" \ "title").foreach(n =>
+      println(n.text)
+    )
+    println(javaVersion)
+    println(javaTitle.text)
 
-        //  NodeBuffer
-        var nb = new NodeBuffer
-        nb += <language>Java</language>
-        nb += <language>Groovy</language>
-        nb = nb &+ <language>Scala</language> &+ <language>Kotlin</language>
-        val langs3 =
-            <langs>
-                {nb}
-            </langs>
-        println(printer.format(xml))
-
-        //  Output
-        val doctype = DocType("html",
-            PublicID("-//W3C//DTD XHTML 1.0 Strict//EN",
-                "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"),
-            Nil)
-        XML.save("files/langs.xml", langs3,
-            "utf-8", xmlDecl = true, doctype)
-
-        //        val doctype = DocType("html",
-        //              PublicID("-//W3C//DTD XHTML 1.0 Strict//EN",
-        //                "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"),
-        //              Nil)
-        //    scala.xml.XML.save("coffeetime-scala/outputs/portfolio.xml", portfolio, "utf-8", true, doctype)
-        //
-    }
-
-    def parseXml(): Unit = {
-        println("========invoke parseXml()========")
-
-        val langs =
-            <langs type='current' count='4' mainstream='true'>
-                <language flavor='static' version='1.8.0_25'>Java</language>
-                <language flavor='dynamic' version='2.4.4'>Groovy</language>
-                <language flavor='static' version='2.11.5'>Scala</language>
-                <language flavor='static' version='0.12.613'>Kotlin</language>
-            </langs>
-
-        
-
-        //        val weather =
-        //      <rss>
-        //        <channel>
-        //          <title>Yahoo! Weather - Boulder, CO</title>
-        //          <item>
-        //            <title>Conditions for Boulder, CO at 2:54 pm MST</title>
-        //            <forecast day="Thu" date="10 Nov 2011" low="37" high="58" text="Partly Cloudy"
-        //                      code="29"/>
-        //            <forecast day="Fri" date="11 Nov 2011" low="39" high="58"
-        //                      text="Mostly Cloudy" code="28"/>
-        //            <forecast day="Sat" date="12 Nov 2011" low="32" high="49" text="Cloudy"
-        //                      code="27"/>
-        //          </item>
-        //        </channel>
-        //      </rss>
-        //
-        //    val forecast = (weather \ "channel" \ "item" \ "forecast")(0)
-        //    val day = forecast \ "@day"
-        //    val low = (weather \\ "forecast")(0) \ "@low"
-        //    println(forecast) //<forecast day="Thu" date="10 Nov 2011" low="37" high="58" text="Partly Cloudy" code="29"/>
-        //    println(day, day.getClass) //(Thu,class scala.xml.Group)
-        //    println(day.text, day.text.getClass) //(Thu,class java.lang.String)
-        //    println(low) //37
-    }
+  }
 }
