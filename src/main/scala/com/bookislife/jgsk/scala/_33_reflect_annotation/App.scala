a3ddffa9df118a9cdfea803880a189f5c659fae0
sidneyy <siriuseddy@gmail.com>
2015-11-26 18:23:30 +0800

    Update scala reflect.

--- src/main/scala/com/bookislife/jgsk/scala/_33_reflect_annotation/App.scala
+++ src/main/scala/com/bookislife/jgsk/scala/_33_reflect_annotation/App.scala
@@ -1,36 +1,44 @@
 package com.bookislife.jgsk.scala._33_reflect_annotation
 
 import scala.annotation.{ClassfileAnnotation, StaticAnnotation}
+import scala.reflect.runtime.universe._
 
 /**
-  * Created by SidneyXu on 2015/11/25.
-  */
+ * Created by SidneyXu on 2015/11/25.
+ */
 object App {
 
-    def main(args: Array[String]) {
-        val person = new Person
-        person.age = 20
+  def main(args: Array[String]) {
+    val person = new Person
+    person.age = 20
 
-        val clazz = classOf[Person]
-        val bean = clazz.getAnnotations.find(anno => {
-            anno.annotationType().getTypeName == classOf[Bean].getCanonicalName
-        }).asInstanceOf[Bean]
-        println(s"name is ${bean.name}")
+    val bean = typeOf[Person].typeSymbol.annotations.find(anno => {
+      anno.tree.tpe == typeOf[Bean]
+    }).get
+    println(bean)
+
+  }
+
+  @Bean(name = "t_person")
+  class Person {
+    @BeanField
+    private var privateAge: Int = 0
+
+    @BeanMethod(alias = "trueAge")
+    def age_=(pAge: Int) {
+      privateAge = pAge
     }
 
-}
+    def age = privateAge
 
-@Bean(name = "t_person")
-class Person {
-    @Bean(name = "f_age")
-    var age: Int = 0
-
+    @BeanMethod
     def sayHello(message: String) = println(s"hello $message")
+  }
+
+  class Bean(val name: String) extends ClassfileAnnotation
+
+  class BeanField extends StaticAnnotation
+
+  class BeanMethod(val alias: String = "") extends StaticAnnotation
+
 }
-
-//http://stackoverflow.com/questions/2265773/how-do-you-define-an-interface-in-scala
-class Bean(val name: String) extends ClassfileAnnotation
-
-class BeanField extends StaticAnnotation
-
-class BeanMethod(val alias: String = "") extends StaticAnnotation
