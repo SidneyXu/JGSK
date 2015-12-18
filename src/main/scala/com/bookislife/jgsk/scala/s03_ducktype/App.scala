1cc10feaf9b1f2cdb082e5dbaff483bb212a1735
SidneyXu <siriuseddy@gmail.com>
2015-12-18 08:05:52 +0800

    Update.

--- src/main/scala/com/bookislife/jgsk/scala/s03_ducktype/App.scala
+++ src/main/scala/com/bookislife/jgsk/scala/s03_ducktype/App.scala
@@ -5,32 +5,36 @@ package com.bookislife.jgsk.scala.s03_ducktype
   */
 object App {
 
-  def main(args: Array[String]) {
-    val peter = new Person("Peter")
-    show(peter) //  name is Peter
+    def main(args: Array[String]) {
+        val peter = new Person("Peter")
+        show(peter) //  name is Peter
 
-    val dollar = new Currency("Dollar")
-    show(dollar) //  name is Dollar
+        val dollar = new Currency("Dollar")
+        show(dollar) //  name is Dollar
 
-    val jane = new Person("Jane")
-    show2(peter, jane) //  Peter and Jane are friends.
-    //  Type mismatch
-    //    show2(dollar, jane)
-  }
+        val jane = new Person("Jane")
+        show2(peter, jane) //  Peter and Jane are friends.
+        //  Type mismatch
+        //    show2(dollar, jane)
 
-  def show(obj: {val name: String}): Unit = {
-    println(s"name is ${obj.name}")
-  }
+        show(new {
+            val name = "foobar"
+        })
+    }
 
-  def show2(obj: {def makeFriend(p: Person)}, p: Person): Unit = {
-    obj.makeFriend(p)
-  }
+    def show(obj: {val name: String}): Unit = {
+        println(s"name is ${obj.name}")
+    }
+
+    def show2(obj: {def makeFriend(p: Person)}, p: Person): Unit = {
+        obj.makeFriend(p)
+    }
 }
 
 class Person(val name: String) {
-  def makeFriend(p: Person): Unit = {
-    println(s"$name and ${p.name} are friends.")
-  }
+    def makeFriend(p: Person): Unit = {
+        println(s"$name and ${p.name} are friends.")
+    }
 }
 
 class Currency(val name: String)
